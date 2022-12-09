b921aff64aac40c60144d8f22fb54aee593b81bc
James MacDonald <james.macdonald@heanet.ie>
2022-12-09 14:13:57 +0000

    lab 6

--- lab_6.sh
+++ lab_6.sh
@@ -6,3 +6,7 @@ apt install python3-pip git pwgen unzip vim
 pip3 install pip --upgrade
 pip3 install requests==2.14.2
 pip3 install docker-compose
+
+wget https://github.com/ansible/awx/archive/17.0.0.zip
+unzip 17.0.0.zip
+cd awx-17.0.0/installer
